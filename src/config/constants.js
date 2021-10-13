4a5bf3fda106db75530ac6ccf5da695076eee769
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 15:32:19 +0200

    add icons for resources

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,21 @@
+import {
+  LocalMovies,
+  People,
+  Language,
+  Accessibility,
+  Flight,
+  DirectionsCarFilled,
+} from '@mui/icons-material';
+
+export const ICONS = {
+  films: <LocalMovies />,
+  people: <People />,
+  planets: <Language />,
+  species: <Accessibility />,
+  starships: <Flight />,
+  vehicles: <DirectionsCarFilled />,
+};
+
 export const API_URL = 'http://localhost:8080/api/';
 
 export const RESOURCES = [
