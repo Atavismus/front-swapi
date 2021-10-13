8dd7c004116808878455762d8ddcba327d6ff4a7
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 21:31:39 +0200

    remove console.log

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -2,6 +2,5 @@ import { SWAPI_URL } from '../config/constants';
 
 export const getIdFromUrl = (url) => {
   const id = url.slice(0, -1).replace(SWAPI_URL, '').split('/');
-  console.log(id[1]);
   return id[1];
 };
