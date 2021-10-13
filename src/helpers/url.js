079709a0b96670024686ea6c1be77251524173f0
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 22:09:38 +0200

    fix & update helper url

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -2,5 +2,9 @@ import { SWAPI_URL } from '../config/constants';
 
 export const getIdFromUrl = (url) => {
   const id = url.slice(0, -1).replace(SWAPI_URL, '').split('/');
-  return id[1];
+  return parseInt(id[1]);
+};
+
+export const getPathFromUrl = (url) => {
+  return url.replace(SWAPI_URL, '');
 };
