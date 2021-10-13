1bdbeb152b15fcb6c27150d44af348b976ee3920
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 11:23:34 +0200

    add class Vehicle

---
+++ src/classes/Vehicle.js
@@ -0,0 +1,34 @@
+export class Vehicle {
+  constructor(object = {}) {
+    this.name = object.name;
+    this.model = object.model;
+    this.vehicle_class = object.vehicle_class;
+    this.manufacturer = object.manufacturer;
+    this.length = object.length;
+    this.cost_in_credits = object.cost_in_credits;
+    this.crew = object.crew;
+    this.passengers = object.passengers;
+    this.max_atmosphering_speed = object.max_atmosphering_speed;
+    this.cargo_capacity = object.cargo_capacity;
+    this.consumables = object.consumables;
+    this.films = object.films;
+    this.pilots = object.pilots;
+  }
+  getTitleFields() {
+    return [this.name, this.model, this.vehicle_class];
+  }
+  getSheetFields() {
+    return {
+      manufacturer: 'Manufacturer',
+      cost_in_credits: 'Cost in credits',
+      length: 'Length',
+      crew: 'Crew',
+      passengers: 'Passengers',
+      max_atmosphering_speed: 'Max atmosphering speed',
+      cargo_capacity: 'Cargo capacity',
+      consumables: 'Consumables',
+      films: 'Films',
+      pilots: 'Pilots',
+    };
+  }
+}
