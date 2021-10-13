3f8463a780cbd9f358ee33758600ffe1d94b3188
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 11:26:25 +0200

    add array of classes for dynamic instantiation

---
+++ src/classes/classes.js
@@ -0,0 +1,15 @@
+import { Film } from './Film';
+import { People } from './People';
+import { Planet } from './Planet';
+import { Specie } from './Specie';
+import { Starship } from './Starship';
+import { Vehicle } from './Vehicle';
+
+export const classes = {
+  films: Film,
+  people: People,
+  planets: Planet,
+  species: Specie,
+  starships: Starship,
+  vehicles: Vehicle,
+};
