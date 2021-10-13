7883587ef6008c6f42bc24189929e920184f40ca
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 11:16:48 +0200

    add class Starship

---
+++ src/classes/Starship.js
@@ -0,0 +1,38 @@
+export class Starship {
+  constructor(object = {}) {
+    this.name = object.name;
+    this.model = object.model;
+    this.starship_class = object.starship_class;
+    this.manufacturer = object.manufacturer;
+    this.cost_in_credits = object.cost_in_credits;
+    this.length = object.length;
+    this.crew = object.crew;
+    this.passengers = object.passengers;
+    this.max_atmosphering_speed = object.max_atmosphering_speed;
+    this.hyperdrive_rating = object.hyperdrive_rating;
+    this.MGLT = object.MGLT;
+    this.cargo_capacity = object.cargo_capacity;
+    this.consumables = object.consumables;
+    this.films = object.films;
+    this.pilots = object.pilots;
+  }
+  getTitleFields() {
+    return [this.name, this.model, this.starship_class];
+  }
+  getSheetFields() {
+    return {
+      manufacturer: 'Manufacturer',
+      cost_in_credits: 'Cost in credits',
+      length: 'Length',
+      crew: 'Crew',
+      passengers: 'Passengers',
+      max_atmosphering_speed: 'Max atmosphering speed',
+      hyperdrive_rating: 'Hyperdrive rating',
+      MGLT: 'MGLT',
+      cargo_capacity: 'Cargo capacity',
+      consumables: 'Consumables',
+      films: 'Films',
+      pilots: 'Pilots',
+    };
+  }
+}
