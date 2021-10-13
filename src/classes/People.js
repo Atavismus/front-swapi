a828f84e57a60076e2f6fccce048388061d4dc6d
Atavismus <soleilnoirdsr@hotmail.com>
2021-10-13 10:16:02 +0200

    add class People

---
+++ src/classes/People.js
@@ -0,0 +1,34 @@
+export class People {
+  constructor(object = {}) {
+    this.name = object.name;
+    this.birth_year = object.birth_year;
+    this.eye_color = object.eye_color;
+    this.gender = object.gender;
+    this.hair_color = object.hair_color;
+    this.height = object.height;
+    this.mass = object.mass;
+    this.skin_color = object.skin_color;
+    this.homeworld = object.homeworld;
+    this.films = object.films;
+    this.species = object.species;
+    this.starships = object.starships;
+    this.vehicles = object.vehicles;
+  }
+  getTitleFields() {
+    return [this.name, this.homeworld, this.birth_year];
+  }
+  getSheetFields() {
+    return {
+      gender: 'Gender',
+      skin_color: 'Skin color',
+      hair_color: 'Hair color',
+      eye_color: 'Eye color',
+      height: 'Height',
+      mass: 'Mass',
+      films: 'Films',
+      species: 'Species',
+      starships: 'Starships',
+      vehicles: 'Vehicles',
+    };
+  }
+}
